90f1a7837d7c0d860b9954ab3cb73c73ac3870c9
RaphaAmericano <raphael@raphaelamericano.com.br>
2016-10-09 17:48:00 -0300

    First Commit

---
+++ WPpharmaHackSqlCleanQuery.sql
@@ -0,0 +1,15 @@
+--Limpar strings com tags indesejadas
+
+UPDATE wp_db_of_posts SET post_content = REPLACE ( post_content, '<a title="Propranolol 80mg" href="http://buynorx1.com/buy-propranolol">Propranolol 80mg</a>', '');
+UPDATE wp_db_of_posts SET post_content = REPLACE ( post_content, '<a href="http://buynorx1.com">buy propranolol</a>', '');
+UPDATE wp_db_of_posts SET post_content = REPLACE ( post_content, '<a title="buy Paxil" href="http://ordernorxx.com/order-paxil-online">buy Paxil</a>', '');
+UPDATE wp_db_of_posts SET post_content = REPLACE ( post_content, '<a title="cheap albuterol" href="http://buynorx1.com/buy-ventolin-albuterol">cheap albuterol</a>', '');
+UPDATE wp_db_of_posts SET post_content = REPLACE ( post_content, '<a title="order Strattera" href="http://buynorx1.com/buy-strattera">order Strattera</a>', '');
+UPDATE wp_db_of_posts SET post_content = REPLACE ( post_content, '<a title="cheap Lithium" href="http://buynorxx.com/buy-lithium-online">cheap Lithium</a>', '');
+UPDATE wp_db_of_posts SET post_content = REPLACE ( post_content, '<a title="purchase Lamisil online" href="http://buynorxx.com/buy-lamisil-online">purchase Lamisil online</a>', '');
+UPDATE wp_db_of_posts SET post_content = REPLACE ( post_content, '<a href="http://www.pharmacyincity.com/buy-cheap-asacol-mesalamine-online">buy Asacol</a>', '');
+UPDATE wp_db_of_posts SET post_content = REPLACE ( post_content, '<a href="http://buynorx1.com/buy-strattera">cost of Strattera</a>', '');
+UPDATE wp_db_of_posts SET post_content = REPLACE ( post_content, '<a href="http://buynorx1.com/buy-prozac-fluoxetine">order Prozac online</a>', '');
+UPDATE wp_db_of_posts SET post_content = REPLACE ( post_content, '<a href="http://buynorx1.com/buy-ventolin-albuterol">order albuterol</a>', '');
+UPDATE wp_db_of_posts SET post_content = REPLACE ( post_content, '<a href="http://ordernorxx.com/order-Colchicine-online">Colchicine without prescription</a>', '');
+UPDATE wp_db_of_posts SET post_content = REPLACE ( post_content, '<a title="Cheap Lamisil" href="http://buynorxx.com/buy-lamisil-online">Cheap Lamisil</a>', '');
